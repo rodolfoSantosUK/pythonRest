bb1db043dc22e565fec3639849db20d2b9f975b1
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-04-04 08:44:57 -0300

    Fazendo uma requisição POST

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
+
+
+@app.route('/add_two_numbers', methods=["GET","POST"])
+def add_two_nums():
+    dataDict = request.get_json()
+
+    if "y" not in dataDict:
+        return "ERROR", 305
+
+    x = dataDict["x"]
+    y = dataDict["y"]
+
+    z = x +y
+
+    retJSON = {
+        "z":z
+    }
+
+    return jsonify(retJSON), 200
 
 @app.route('/')
 def hello_world():
     return "Hello World!"
 
+@app.route('/hithere')
+def hi_there_everyone ():
+    return 'I just hit /hithere'
+
+@app.route('/bye')
+def bye():
+     c=2*534
+     s=str(c)
+     return bye ;
+
 if __name__=="main":
-    app.run()
+    app.run(debug=True)
 
   # export FLASK_APP = app.py
   # flask run
